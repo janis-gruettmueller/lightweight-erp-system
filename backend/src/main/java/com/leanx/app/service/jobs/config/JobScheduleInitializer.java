f24d80e74e705d2c4ebb10d50d7c30ac98056fc2
Janis Gruettmueller <janis.gr@outlook.com>
2025-03-31 12:02:26 +0200

    fix: adjusted job schedule

--- backend/src/main/java/com/leanx/app/service/jobs/config/JobScheduleInitializer.java
+++ backend/src/main/java/com/leanx/app/service/jobs/config/JobScheduleInitializer.java
@@ -38,7 +38,7 @@ public class JobScheduleInitializer implements ServletContextListener {
                     .withIdentity("onboardingJob", "hr-ops")
                     .build();
 
-            // Define Trigger (Runs every day at 10:00 UTC)
+            // Define Trigger (Runs every day at 10:15 UTC)
             /* Cron Expressions: 
                 *  *  *  *  *  *  *
                 |  |  |  |  |  |  |-- Year (optional)
@@ -58,7 +58,7 @@ public class JobScheduleInitializer implements ServletContextListener {
             */ 
             Trigger trigger = TriggerBuilder.newTrigger()
                     .withIdentity("dailyTrigger", "hr-ops")
-                    .withSchedule(CronScheduleBuilder.cronSchedule("0 0 10 * * ?")) // Run at 10:00 UTC every day
+                    .withSchedule(CronScheduleBuilder.cronSchedule("0 15 10 * * ?")) // Run at 10:15 UTC every day
                     .build();
 
             // Schedule the job
